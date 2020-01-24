e55c055f107239aad999df55b7b901fa2893a5eb
Daniel Pacak <pacak.daniel@gmail.com>
2020-01-24 16:33:13 +0100

    feat: Add Descriptor Scan Report (#5)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

---
+++ pkg/data/repository.go
@@ -0,0 +1,72 @@
+package data
+
+import (
+	"context"
+	"encoding/json"
+	security "github.com/danielpacak/k8s-security-crds/pkg/apis/security/v1"
+	"github.com/vmware-tanzu/octant/pkg/plugin/service"
+	"github.com/vmware-tanzu/octant/pkg/store"
+	"k8s.io/apimachinery/pkg/labels"
+)
+
+type Repository struct {
+	client service.Dashboard
+}
+
+func NewRepository(client service.Dashboard) *Repository {
+	return &Repository{
+		client: client,
+	}
+}
+
+func (r *Repository) GetImageScanReportFor(ctx context.Context, imageRef string) (*security.ImageScanReport, error) {
+	unstructuredList, err := r.client.List(ctx, store.Key{
+		APIVersion: "security.danielpacak.github.com/v1",
+		Kind:       "ImageScanReport",
+		Selector: &labels.Set{
+			"image-ref": imageRef,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	b, err := unstructuredList.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	var reportList security.ImageScanReportList
+	err = json.Unmarshal(b, &reportList)
+	if err != nil {
+		return nil, err
+	}
+	if len(reportList.Items) == 0 {
+		return nil, nil
+	}
+	return &reportList.Items[0], nil
+}
+
+func (r *Repository) GetDescriptorScanReportFor(ctx context.Context, imageRef string) (*security.DescriptorScanReport, error) {
+	unstructuredList, err := r.client.List(ctx, store.Key{
+		APIVersion: "security.danielpacak.github.com/v1",
+		Kind:       "DescriptorScanReport",
+		Selector: &labels.Set{
+			"image-ref": imageRef,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	b, err := unstructuredList.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	var reportList security.DescriptorScanReportList
+	err = json.Unmarshal(b, &reportList)
+	if err != nil {
+		return nil, err
+	}
+	if len(reportList.Items) == 0 {
+		return nil, nil
+	}
+	return &reportList.Items[0], nil
+}
