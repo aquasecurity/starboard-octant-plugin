e9a83de18d54c755d4170a7e3cd408be9d400dfe
Daniel Pacak <pacak.daniel@gmail.com>
2020-01-25 00:23:06 +0100

    feat: Use label selectors (#6)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

---
+++ pkg/view/debug.go
@@ -0,0 +1,9 @@
+package view
+
+import "github.com/vmware-tanzu/octant/pkg/view/component"
+
+func NewDebug(s string) component.Component {
+	card := component.NewCard([]component.TitleComponent{component.NewText("Debug")})
+	card.SetBody(component.NewMarkdownText(s))
+	return card
+}
