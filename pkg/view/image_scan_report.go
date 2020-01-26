3bf0c7cc66fc1a65f52d57c556ad2ac7f649edaf
Daniel Pacak <pacak.daniel@gmail.com>
2020-01-26 01:41:42 +0100

    feat: Display image scan report for each container (#7)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/view/image_scan_report.go
+++ pkg/view/image_scan_report.go
@@ -1,31 +1,37 @@
 package view
 
 import (
+	"fmt"
 	security "github.com/danielpacak/k8s-security-crds/pkg/apis/security/v1"
 	"github.com/vmware-tanzu/octant/pkg/view/component"
+	"sort"
+	"strings"
 )
 
-func NewImageScanReport(report *security.ImageScanReport) component.Component {
+func NewImageScanReport(containerName string, report security.ImageScanReport) component.Component {
 	table := component.NewTableWithRows(
-		"Image Scan Report", "Image scan report",
+		fmt.Sprintf("Image Scan Report %s", containerName), "Image scan report",
 		component.NewTableCols("ID", "Severity", "Title", "Resource", "Installed Version", "Fixed Version"),
 		[]component.TableRow{})
 
-	if report != nil {
-		for _, vi := range report.Spec.Vulnerabilities {
-			tr := component.TableRow{
-				"ID":                getLinkComponent(vi),
-				"Severity":          component.NewText(vi.Severity),
-				"Title":             component.NewText(vi.Title),
-				"Resource":          component.NewText(vi.Resource),
-				"Installed Version": component.NewText(vi.InstalledVersion),
-				"Fixed Version":     component.NewText(vi.FixedVersion),
-			}
-			table.Add(tr)
+	vulnerabilities := report.Spec.Vulnerabilities
+
+	sort.SliceStable(vulnerabilities, func(i, j int) bool {
+		return strings.Compare(vulnerabilities[i].Severity, vulnerabilities[j].Severity) < 0
+	})
+
+	for _, vi := range report.Spec.Vulnerabilities {
+		tr := component.TableRow{
+			"ID":                getLinkComponent(vi),
+			"Severity":          component.NewText(vi.Severity),
+			"Title":             component.NewText(vi.Title),
+			"Resource":          component.NewText(vi.Resource),
+			"Installed Version": component.NewText(vi.InstalledVersion),
+			"Fixed Version":     component.NewText(vi.FixedVersion),
 		}
+		table.Add(tr)
 	}
 
-	table.Sort("ID", false)
 	return table
 }
 
